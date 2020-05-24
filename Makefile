30aba21baeef663c49f7744a52966e259d7c8e99
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2020-05-24 02:16:01 -0700

    Fix prometheus output.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ VERSION = $(shell cargo pkgid | sed 's!.*\:!!')
 version:
 	@echo "$(VERSION)"
 run:
-	WEB_EXPORTER_LOG_LEVEL=warn cargo run
+	WEB_EXPORTER_LOG_LEVEL=info cargo run
 # before releasing update version in cargo.toml file and run prerelease.
 prerelease:
 	cargo generate-lockfile
--- src/results.rs
+++ src/results.rs
@@ -25,28 +25,28 @@ impl fmt::Display for TargetResult {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut results = Vec::new();
         let duration_stat = format!(
-            "web_exporter_response_duration_milliseconds{{url=\"{}\", status={}, error={}}} {}",
+            "web_exporter_response_duration_milliseconds{{url=\"{}\", status=\"{}\", error=\"{}\"}} {}",
             self.url,
             self.status,
-            if self.error { 1 } else { 0 },
+            self.error, // if self.error { 1 } else { 0 },
             self.duration,
         );
         let size_stat = format!(
-            "web_exporter_response_response_size_bytes{{url=\"{}\", status={}, error={}}} {}",
+            "web_exporter_response_response_size_bytes{{url=\"{}\", status=\"{}\", error=\"{}\"}} {}",
             self.url,
             self.status,
-            if self.error { 1 } else { 0 },
+            self.error, // if self.error { 1 } else { 0 },
             self.size,
         );
         results.push(duration_stat);
         results.push(size_stat);
         for query_result in &self.query_results {
             let query_stat = format!(
-                "web_exporter_query_count{{url=\"{}\", query=\"{}\", status={}, error={}}} {}",
+                "web_exporter_query_count{{url=\"{}\", query=\"{}\", status=\"{}\", error=\"{}\"}} {}",
                 self.url,
                 query_result.query,
                 self.status,
-                if self.error { 1 } else { 0 },
+                self.error, // if self.error { 1 } else { 0 },
                 // If we cannot parse selector just return -1
                 query_result.count.unwrap_or(0),
             );
